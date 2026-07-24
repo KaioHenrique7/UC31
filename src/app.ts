ae4aac23d448bed7c1021a5a9126ab7b99622178
MARIA LETICIA FIDELIS RODRIGUES <a95717223@rn.senac.local>
2026-07-24 14:10:16 -0300

    routes prontas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+import clientesRoutes from "./routes/clientes";
+import veiculosRoutes from "./routes/veiculos";
+import ordensRoutes from "./routes/ordens";
+import authRoutes from "./routes/auth";
+
+app.use("/clientes", clientesRoutes);
+app.use("/veiculos", veiculosRoutes);
+app.use("/ordens", ordensRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000');
