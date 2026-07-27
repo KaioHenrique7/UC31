6a536ba0e19a2b8af8cce4c826b2c76c3030c68b
MARIA LETICIA FIDELIS RODRIGUES <a95717223@rn.senac.local>
2026-07-27 17:23:20 -0300

    Clientecontrollers

---
+++ src/controllers/ClienteController.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { ClienteRepository } from "../models/ClienteRepository";
+
+const router = Router();
+const repository = new ClienteRepository();
+
+router.get("/", async (req, res) => {
+    const clientes = await repository.listar();
+    res.json(clientes);
+});
+
+router.get("/:id", async (req, res) => {
+    const cliente = await repository.buscarPorId((req.params.id));
+
+    if (!cliente) {
+        return res.status(404).json({ mensagem: "Cliente não encontrado." });
+    }
+
+    res.json(cliente);
+});
+
+router.post("/", async (req, res) => {
+    const cliente = await repository.criar(req.body);
+    res.status(201).json(cliente);
+});
+
+router.put("/:id", async (req, res) => {
+    const cliente = await repository.atualizar((req.params.id), req.body);
+    res.json(cliente);
+});
+
+router.delete("/:id", async (req, res) => {
+    await repository.remover((req.params.id));
+    res.sendStatus(204);
+});
+
+export default router;
